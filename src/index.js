877067e888f8187d248f7e14a19722052ceb567d
Dana Zinatbakhsh <dtzinatbakhsh@gmail.com>
2019-11-04 11:29:54 -0800

    fixed error pertaining to test code curriculum updated probably

--- src/index.js
+++ src/index.js
@@ -8,25 +8,26 @@ import api from "./api";
 import store from "./store";
 
 const main = function() {
-  // api
-  //   .createItem("pears")
-  //   .then(res => res.json())
-  //   .then(newItem => {
-  //     return api.getItems();
-  //   })
-  //   .then(res => res.json())
-  //   .then(items => {
-  //     console.log(items);
-  //   });
-  api.getItems().then(res => {
-    console.log(api.BASE_URL);
-    const item = store.items[0];
-    console.log(item);
-    console.log("current name: " + item.name);
-    store.findAndUpdate(item.id, { name: "foobar" });
-    console.log("new name: " + item.name);
-  });
+ 
+  api.getItems()
+    .then(res => res.json())
+    .then((items) => {
+      console.log(api.BASE_URL);
+      store.items = items;
+      const item = store.items[0];
+      console.log(item);
+      console.log("current name: " + item.name);
+      store.findAndUpdate(item.id, { name: "foobar" });
+      console.log("new name: " + item.name);
+    })
+    .then(res => res.json())
+    .then(() => console.log('updated!'));
+         
+      
+    
 
+
+  
   shoppingList.bindEventListeners();
   shoppingList.render();
 };
